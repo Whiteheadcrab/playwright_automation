001df26b290c6cefc29a6442d52aa71e2897a7c2
Mykyta Bieliaiev <bieliaiev.mykyta@gmail.com>
2025-11-29 17:40:10 +0100

    Added test @SP Place the order

---
+++ tests/WebAPIPart2Test.spec.js
@@ -0,0 +1,43 @@
+const { test, expect, request } = require('@playwright/test');
+const { APiUtils } = require('./utils/APiUtils');
+const loginPayLoad = { userEmail: "mike.testman@mailinator.com", userPassword: "Team123!" };
+const orderPayLoad = { orders: [{ country: "India", productOrderedId: "68a961459320a140fe1ca57a" }] };
+const fakePayLoadOrders = { data: [], message: "No Orders" };
+ 
+let response;
+test.beforeAll(async () => {
+  const apiContext = await request.newContext();
+  const apiUtils = new APiUtils(apiContext, loginPayLoad);
+  response = await apiUtils.createOrder(orderPayLoad);
+ 
+})
+ 
+ 
+//create order is success
+test('@SP Place the order', async ({ page }) => {
+  page.addInitScript(value => {
+ 
+    window.localStorage.setItem('token', value);
+  }, response.token);
+  await page.goto("https://rahulshettyacademy.com/client");
+ 
+ 
+  // move to orders cart via route
+  await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
+    async route => {
+      const response = await page.request.fetch(route.request());
+      let body = JSON.stringify(fakePayLoadOrders);
+      route.fulfill(
+        {
+          response,
+          body, 
+ 
+        });
+      //intercepting response -APi response-> { playwright fakeresponse}->browser->render data on front end
+    });
+ 
+  await page.locator("button[routerlink*='myorders']").click();
+  await page.waitForResponse("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*")
+ 
+  console.log(await page.locator(".mt-4").textContent());
+});
